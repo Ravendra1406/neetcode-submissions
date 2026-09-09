f7c660de38bc16ae98f5374e3a5eeec16aafb5e4
Ravendra1406 <78633783+Ravendra1406@users.noreply.github.com>
2026-09-09 17:10:29 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n=position.size();
+        vector<pair<int,int>>temp;
+        for(int i=0;i<n;i++)
+        {
+            temp.push_back({position[i],speed[i]});
+        }
+        sort(temp.begin(),temp.end(),greater<pair<int,int>>());
+        float mx=0;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+          float t=target-temp[i].first;
+          t=(float)t/(temp[i].second);
+          if(t>mx)
+          {ans++;
+          mx=t;
+          }
+        }
+        return ans;
+    }
+};
